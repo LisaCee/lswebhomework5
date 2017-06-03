9edd03916eea0b8370d536803000d4af1d725922
Lisa Cee <LisaCee@users.noreply.github.com>
2017-06-03 14:31:41 -0700

    Add files via upload
    Completed script.js assignment

--- script.js
+++ script.js
@@ -28,25 +28,69 @@ for(var i = 0; i <= 130; i  += 3){
 }
 
 //10. Create a for loop that counts from 3 to 17 by 2s
+for (var i = 3; i <= 17; i += 2){
+	console.log(i);
+}
 
 //11. Create a for loop that counts from 100 to 3 by -1
+for (var i = 100; i >= 3; i--){
+	console.log(i)
+}
 
 //12. Create a for loop that counts from 1 to 100 by 1s
+for (var i = 1; i <= 100; i++){
+	console.log(i);
+}
 
 //13. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizz` if the number is divisible by 5
+for (var i = 1; i < 100; i++){
+	if(i % 5 === 0){
+		console.log("fizz");
+	}
+	else {
+		console.log(i);
+	}
+}
 
 //14. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `buzz` if the number is divisible by 3
+for (var i = 1; i <= 100; i++){
+	if (i % 3 === 0){
+		console.log("buzz");
+	}
+	else {
+		console.log(i);
+	}
+}
 
 //15. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizzbuzz` if the number is divisible by 15
+for (var i = 1; i <= 100; i++){
+	if (i % 15 === 0){
+		console.log("fizzbuzz");
+	}
+	else {
+		console.log(i);
+	}
+}
 
 //EXTRA CREDIT: Fizzbuzz
 
 /*
-
 The "Fizz-Buzz test" is an interview question designed to help filter out the 99.5% of programming job candidates who can't seem to program their way out of a wet paper bag. The text of the programming assignment is as follows:
-
 "Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
+Hint: Use your last three loops and if/then/else statements. You can learn about those here https://www.w3schools.com/js/js_if_else.asp*/
 
-Hint: Use your last three loops and if/then/else statements. You can learn about those here https://www.w3schools.com/js/js_if_else.asp
+for (var i = 1; i <= 100; i++){
+	if (i % 3 === 0 && i % 5 === 0){
+		console.log("Fizzbuzz");
+	}
+	else if (i % 3 === 0){
+		console.log("Fizz");
+	}
+	else if (i % 5 === 0){
+		console.log("Buzz");
+	}
+	else{
+		console.log(i);
+	}
 
-*/
+}
